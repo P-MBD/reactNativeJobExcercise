35df9800878db0593352a28943d09b45f1685e5b
unknown <p.mobedi@gmail.com>
2024-02-16 03:26:59 -0800

    expo font

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,27 @@
-import {Stack} from 'expo-router';
-import {View, ScrollView, SafeAreaView} from 'react-native';
-const Layout = () => {
-    return <Stack />
+import { Stack } from "expo-router";
+import { useCallback } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Inter-Black': require('../assets/fonts/DMSans-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <Stack onLayout={onLayoutRootView}/>
+  );
 }
-export default Layout;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,11 +1,21 @@
+import { useRouter } from 'expo-router';
 import React from 'react';
-import { View, Text} from "react-native";
-
+import { View, Text,TextInput} from "react-native";
+import styles from './welcome.style';
 
 const Welcome = () => {
+  const router = useRouter();
   return(
     <View>
-        <Text>Welcome</Text>
+        <View style={styles.container}>
+            <Text style={styles.userName}> Hello Adrian</Text>
+            <Text style={styles.welcomeMessage}> Find your perfect job</Text>
+        </View>
+        <View style={styles.searchWrapper}>
+            <View style={styles.searchContainer}>
+                <TextInput style={styles.searchInput} />
+            </View>
+        </View>
     </View>
   );
 };
